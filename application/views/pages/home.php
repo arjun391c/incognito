cecc7fbb9f9add25b53e06501988bb04f5d4198e
Arjun C <54018138+arjun391c@users.noreply.github.com>
2020-02-28 14:39:29 +0530

    Update home.php

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -13,7 +13,7 @@
 
 			<h3 class="Dagger colwhite ">Sponsored By</h3>
 
-			<img class="banner img-fluid" width="42" src="<?php echo base_url('assets/img/sponsor.png');?>"> </img>
+			<img class=" img-fluid" width="42" src="<?php echo base_url('assets/img/sponsor.png');?>"> </img>
 
 		</div>
 
