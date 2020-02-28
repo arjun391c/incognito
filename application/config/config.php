30e58ab92b91a4b4a5b66e98e86bfb8f522bf619
Arjun C <54018138+arjun391c@users.noreply.github.com>
2020-02-28 14:08:19 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://hesnhta.mrdev.co.in/';
+$config['base_url']	= 'https://incognito.hestia.live/';
 
 /*
 |--------------------------------------------------------------------------
